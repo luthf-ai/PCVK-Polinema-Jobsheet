6d32a8a3b8ad69792025cdddb08232b1b46a330e
luthf-ai <aido.el.hakim@gmail.com>
2023-11-27 21:46:36 +0700

    fixi yo yolo, reorganize, webapp

--- quiz2/yolo/main.py
+++ quiz2/yolo/main.py
@@ -45,8 +45,8 @@ def detect_objects_on_image(buf):
     :return: Array of bounding boxes in format 
     [[x1,y1,x2,y2,object_type,probability],..]
     """
-    model = YOLO("best.pt")
-    results = model.predict(buf)
+    model = YOLO("kontrakan.pt")
+    results = model.predict(buf,conf=0.9)
     result = results[0]
     output = []
     for box in result.boxes:
